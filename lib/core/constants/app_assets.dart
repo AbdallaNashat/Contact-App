6bead2cea9ec6adef82257c53b73fd3087fff17a
AbdallaNashat <abdonashat42@gmail.com>
2025-06-28 17:03:05 +0300

    Initial commit

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,8 @@
+abstract class AppAssets{
+  static const String splashLogo = "assets/images/Contact.png";
+  static const String contactLogo = "assets/images/ContactLogo.png";
+
+  static const String emptyList = "assets/animations/empty_list.json";
+  //static const String imagePicker = "assets/animations/image_picker.json";
+
+}
